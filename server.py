403a33fdebe74d2d5f54f727fe66e1f613277847
David P. Janes <davidjanes@davidjanes.com>
2014-10-22 08:17:55 -0400

    ...

--- server.py
+++ server.py
@@ -7,33 +7,100 @@
 #   Listen for Mandrill mail connections
 #   and save to a file
 
-import bottle
 import json
 import sys
+import smtplib
 
-cfgd = {
-    "host": '0.0.0.0',
-    "port": 9090,
-}
+import bottle
+import bunch
 
-@bottle.route('/inbound', method='POST')
-def inbound_POST():
-    bottle.response.content_type = "text/plain"
+class Server(object):
+    def __init__(self, cfgd):
+        self.cfgd = bunch.bunchify(cfgd)
+        self._setup_delivery()
 
-    mds = request.forms.get('mandrill_events')
-    if not mds:
-        bottle.response.status = 400
+        self.deliver_otherwise = None
+        self.deliver_emaild = {}
 
-        return "400 Error: Expected 'mandrill_events'"
+    def _setup_delivery(self):
+        pass
 
-    print(sys.stderr, json.dumps(mds, sort_keys=True, indent=2))
-    return "ok"
+    def run(self):
+        bottle.route("/inbound", method="POST")(self.inbound)
+        bottle.run(host=self.cfgd.inbound.host, port=self.cfgd.inbound.port)
 
-@bottle.route('/inbound')
-def inbound():
-    bottle.response.content_type = "text/plain"
-    bottle.response.status = 405
+    def inbound(self):
+        """This receives the email from Mandrill"""
 
-    return "405 Error: Try again with POST"
+        bottle.response.content_type = "text/plain"
 
-bottle.run(host=cfgd['host'], port=cfgd['port'])
+        mds_raw = bottle.request.forms.get('mandrill_events')
+        if not mds_raw:
+            bottle.response.status = 400
+
+            return "400 Error: Expected 'mandrill_events'"
+
+        mds = json.loads(mds_raw)
+        for md in mds:
+            self.routing(md["msg"])
+
+        return "ok"
+
+    def routing(self, msgd):
+        """This routes the email to the proper address(es)"""
+
+        msg_rcpt_email = msgd["email"]
+        msg_from_email = msgd["from_email"]
+        msg_raw = msgd["raw_msg"]
+
+        ## find the message body
+        nlnlx = msg_raw.find("\n\n")
+        crnlcrnlx = msg_raw.find("\r\n\r\n")
+        x = min(filter(lambda x: x != -1, [ nlnlx, crnlcrnlx, len(msg_raw), ]))
+        msg_raw_body = msg_raw[x:]
+
+        ## headers
+        headers = []
+        for tag, value in msgd["headers"].iteritems():
+            tag = tag.lower()
+            if tag.find("yahoo") > -1:
+                continue
+            elif tag.find("signature") > -1:
+                continue
+
+            if isinstance(value, list):
+                headers.extend(value)
+            else:
+                headers.append(value)
+
+        msg_raw_new = "\n".join(headers) + msg_raw_body
+
+        if self.cfgd.routing.default:
+            self.send(msg_from_email, [ self.cfgd.routing.default, ], msg_raw_new)
+
+    def send(self, msg_from_email, msg_rcpts, msg_raw):
+        """This sends the email to Mandrill for final delivery"""
+
+        if self.cfgd.outbound.get('ssl', False):
+            server = smtplib.SMTP_SSL(
+                host=self.cfgd.outbound.host, 
+                port=self.cfgd.outbound.port,
+            )
+        else:
+            server = smtplib.SMTP(
+                host=self.cfgd.outbound.host,
+                port=self.cfgd.outbound.port,
+            )
+
+        server.set_debuglevel(1)
+
+        server.login(self.cfgd.outbound.username, self.cfgd.outbound.password)
+        server.sendmail(msg_from_email, msg_rcpts, msg_raw)
+        server.quit()
+
+if __name__ == '__main__':
+    with open('server.json', 'r') as cin:
+        cfgd = json.load(cin)
+
+    server = Server(cfgd)
+    server.run()
